e6a41727ae6b1e05016514167cfcff3c39c61992
IKShakik <ibrahim.khalil@surbanajurong.com>
2023-12-25 12:54:51 +0600

    End of Section 4

---
+++ Application/Activities/List.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Qeury : IRequest<List<Activity>>{}
+
+        public class Handler : IRequestHandler<Qeury, List<Activity>>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+            public async Task<List<Activity>> Handle(Qeury request, CancellationToken cancellationToken)
+            {
+                return await _context.Activities.ToListAsync();
+            }
+        }
+    }
+}
